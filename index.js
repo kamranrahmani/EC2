8d298e56f9a33b315a813074cc458a396733b318
Kamran Rahmani <Kamran@tcgmachines.com>
2025-02-26 13:33:06 -0500

    added a new route handler

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get("/home", (req, res) => {
   res.send("this is the home page");
 });
 
-app.post("/post", (req, res) => {});
+app.get("/post", (req, res) => {
+  res.send("this is the new endpoint.");
+});
 
 app.listen(3000, () => {
   console.log("server is ready at port 3000");
